a31fa7516cc8cd9f58a5f89feee15124435473f4
ankurg132 <ankurg052@gmail.com>
2021-02-26 02:02:44 +0530

    pattern 1 module solutions

---
+++ pattern 1/interesting_alphabet.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        /*
+         * Your class should be named Solution. Read input as specified in the question.
+         * Print output as specified in the question.
+         */
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int i = 0;
+        while (i < n) {
+            int j = 0;
+            while (j <= i) {
+                char ch = (char) ('A' + n - 1 + j - i);
+                System.out.print(ch);
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+}
